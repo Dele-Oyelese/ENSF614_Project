1f474650efd171cba5ea4c20e89b16860e5d35d3
Dhruvidave1 <dhruvi@ualberta.ca>
2022-11-21 16:00:31 -0700

    minor

--- react-website-movies/src/components/pages/AllMovies.js
+++ react-website-movies/src/components/pages/AllMovies.js
@@ -5,8 +5,7 @@ import React, { useState, useEffect } from 'react';
 function AllCourses() {
     const [courses, setCourse] = useState([])
     useEffect(() => {
-        const url = "http://localhost:8080/api/v1/course";
-    
+        const url = "";    
         const fetchData = async () => {
           try {
             const response = await fetch(url);
@@ -23,16 +22,13 @@ function AllCourses() {
     return (
         <>
         <div className='hero-container'>
-        <div className='allCourses'>
-            <h1>Courses currently offered: </h1>
+        <div className='allMovies'>
+            <h1>Movies: </h1>
             <ul>
                 {courses.map(item => (                    
                     <li>                        
-                        <h1> Course name = {item.name} </h1>
-                        <p> Start Date = {item.startTime} </p>
-                        <p>End Date = {item.endTime}</p>
-                        <p>Capacity = {item.capacity}</p>
-                        {/* <p>Pre-requisites = {item.prerequisites}</p> */}
+                        <h1>Movie name = {item.name} </h1>                        
+                        <p>Capacity = {item.capacity}</p>                        
                     </li>
                 ))}
             </ul>
