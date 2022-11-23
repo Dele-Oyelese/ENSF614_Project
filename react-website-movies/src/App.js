c8438bb500e92cc5399a66219f692f8b1026e220
Dhruvidave1 <dhruvi@ualberta.ca>
2022-11-22 19:42:58 -0700

    added systems activity diagram

--- react-website-movies/src/App.js
+++ react-website-movies/src/App.js
@@ -1,22 +1,24 @@
-import Navbar from './components/navbar';
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/pages/Home.js';
-import AllMovies from './components/pages/AllMovies.js';
 
 function App() {
   return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/AllMovies' element={<AllMovies />} />
-          
-        </Routes>
-      </Router>
-
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
