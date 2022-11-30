83be4c16c5e29a9d6ccc2b4df1ce95e3a67db0cd
Ryan_Ahlstrom <Ryan.Ahlstrom@gmail.com>
2022-11-30 12:40:44 -0700

    Initial Implementation for Search Bar Working

--- react-website-movies/src/components/pages/AllMovies.js
+++ react-website-movies/src/components/pages/AllMovies.js
@@ -1,7 +1,7 @@
 import '../../App.css';
+import jsondata from './movie.json';
 
 import React, { useState} from 'react';
-
 
 
 function AllMovies() {
@@ -30,13 +30,18 @@ function AllMovies() {
                 <input type="text" value={movie} onChange={(e) => setMovie(e.target.value)} className="form-control" placeholder="Search for a movie, e.g. Black Panther"></input>                        
                 <button onClick={searchMovie} className="btn btn-dark" type="button">Search</button>                        
             </div>
-
             <p>Display the searched movie with showtimes below:</p>
-
-
-           
-
-
+            <div>
+                {jsondata.filter((val)=>{
+                    if(movie == "") {
+                        return val
+                    } else if (val.movie_name.toLowerCase().includes(movie.toLowerCase())) {
+                        return val
+                    } return "" 
+                }).map((val, key)=>{
+                    return <div>{val.movie_name} </div>;
+                })}
+            </div>
 
         </div>
 
