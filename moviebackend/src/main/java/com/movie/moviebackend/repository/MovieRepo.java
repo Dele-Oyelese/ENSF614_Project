046b45df3ba5493f29888fd31f82f213a990ed6a
e5h <32693617+e5h@users.noreply.github.com>
2022-11-30 16:50:12 -0700

    Started API docs, added movie API basics

--- moviebackend/src/main/java/com/movie/moviebackend/repository/MovieRepo.java
+++ moviebackend/src/main/java/com/movie/moviebackend/repository/MovieRepo.java
@@ -1,4 +1,11 @@
 package com.movie.moviebackend.repository;
 
-public interface MovieRepo {
+import com.movie.moviebackend.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepo extends JpaRepository<Movie, Long> {
+
+
 }
--- moviebackend/src/main/java/com/movie/moviebackend/service/MovieService.java
+++ moviebackend/src/main/java/com/movie/moviebackend/service/MovieService.java
@@ -1,4 +1,81 @@
 package com.movie.moviebackend.service;
 
+import com.movie.moviebackend.model.Movie;
+import com.movie.moviebackend.repository.MovieRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
 public class MovieService {
+
+    private final MovieRepo movieRepo;
+
+    @Autowired
+    public MovieService(MovieRepo movieRepo)
+    {
+        this.movieRepo = movieRepo;
+    }
+
+    /* Get all movies in the database */
+    public List<Movie> getAllMovies()
+    {
+        return movieRepo.findAll();
+    }
+
+    /* Add a new movie to the database */
+    public void addNewMovie(Movie movie)
+    {
+        Optional<Movie> movieById = movieRepo.findById(movie.getId());
+
+        if(movieById.isPresent())
+        {
+            throw new IllegalStateException("Movie already exists.");
+        }
+
+        movieRepo.save(movie);
+    }
+
+    /* Delete a movie from the database */
+    public void deleteMovie(Long id)
+    {
+        boolean exists = movieRepo.existsById(id);
+        if(!exists)
+        {
+            throw new IllegalStateException("Movie with id " + id + " does not exist.");
+        }
+        else
+        {
+            movieRepo.deleteById(id);
+        }
+    }
+
+    /* Update a movie in the database */
+    @Transactional
+    public void updateMovie(Long id, String title, String showTime)
+    {
+        Movie movie = movieRepo.findById(id).orElseThrow(
+                () -> new IllegalStateException("Movie with id " + id + " does not exist.")
+        );
+
+        /* Check to see if the title is acceptable */
+        /* All movies with this title will be changed. */
+        if(title != null && title.length() > 0 && !Objects.equals(movie.getTitle(), title) )
+        {
+            movie.setTitle(title);
+        }
+
+        /* Check to see if the show time is acceptable */
+        /* Only the movie with matching title and showtime will be changed. */
+        if(showTime != null && !Objects.equals(movie.getShowTime(), showTime) )
+        {
+            /* TODO: Need to figure out this implementation */
+
+            movie.setShowTime(showTime);
+        }
+    }
 }
