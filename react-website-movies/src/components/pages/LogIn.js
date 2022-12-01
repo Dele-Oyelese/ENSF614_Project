4985934f30b5be617363148551b3f30791b28c2b
Dhruvidave1 <dhruvi@ualberta.ca>
2022-12-01 11:39:18 -0700

    login done, goes to all movies now

--- react-website-movies/src/components/pages/LogIn.js
+++ react-website-movies/src/components/pages/LogIn.js
@@ -9,7 +9,6 @@ function LogIn(){
     const[check,setCheck] = React.useState('')
     const[validate,setValidate] = React.useState(false)
     const getEmail = localStorage.getItem("email")
-    const [students,setStudents] = React.useState([])
     let navigate = useNavigate();
 
     const SubmitLogin = (e) => {
@@ -20,13 +19,13 @@ function LogIn(){
 
     React.useEffect(()=> {
         fetch("http://localhost:8080/api/v1/user/Email/".concat(email) )
-        .then(res =>res.json())
+        .then(res => res.json())
         .then((result)=>{
-          if(result.status !== 500  && (result.email ===parseInt(email) )&& result.password === password){
+          if(result.status !== 500  && (result.email === email ) && result.password === password){
             localStorage.setItem("email",result.email)
             const getEmail = localStorage.getItem("email")
             setValidate(true)
-            navigate('/registration');
+            navigate('/allmovies');
           }
           else{
             setCheck(false)
@@ -37,28 +36,21 @@ function LogIn(){
         )
       },[check])
 
-
-
-
-
-
     return(
         <>
         <div className='movie'>
             <form onSubmit={SubmitLogin}>
                 <div class="form-group">
                     <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" placeholder="Enter email"></input>
+                    <input type="email" class="form-control" placeholder="Enter email" value={email} onChange={(e)=>setEmail(e.target.value)}></input>
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" value = {password} onChange={(e)=>setPassword(e.target.value)}></input>
                 </div>
                 <button type="submit" class="btn btn-primary">LogIn</button>
             </form>   
-
-        </div>
-            
+        </div>            
         </>
     );
 }
