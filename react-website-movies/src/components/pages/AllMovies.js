71f94d3881761f56e4abcba24dd64bc76493a3e3
Dhruvidave1 <dhruvi@ualberta.ca>
2022-12-01 18:28:38 -0700

    filter done

--- react-website-movies/src/components/pages/AllMovies.js
+++ react-website-movies/src/components/pages/AllMovies.js
@@ -4,23 +4,25 @@ import React, { useState, useEffect } from 'react';
 
 function AllMovies() {
     const [movie, setMovie] = useState([])
+    const [filtered, setFiltered] = useState([])
     const [val, setVal] = useState('')
     const [search, setSearch] = React.useState('')
     let navigate = useNavigate();
 
-        React.useEffect(()=>{
-            fetch("http://localhost:8080/api/v1/movie")
-            .then(res=>res.json())
-            .then((result)=> {
-              setMovie(result);
+    React.useEffect(() => {
+        fetch("http://localhost:8080/api/v1/movie")
+            .then(res => res.json())
+            .then((result) => {
+                setMovie(result);
             }
             )
-        },[])
+    }, [])
 
     const Search = (e) => {
-        movie.filter(item => {
+        setFiltered(movie.filter(item => {
             return item.title === val;
-        })};
+        }))
+    }
 
     const handleClick = (e) => {
         navigate('/login');
@@ -29,22 +31,42 @@ function AllMovies() {
     return (
 
         <div className='movie'>
-                <div className="input-group">
-                    <input type="text" value={val} onChange={(e) => setVal(e.target.value)} className="form-control" placeholder="Search for a movie, e.g. Black Panther"></input>
-                    <button onClick={Search} className="btn btn-dark" type="button">Search</button>
-                </div>
-        <ul>
-        {Array.isArray(movie)
-            ? movie.map(item => {
-                return (
-                    <><p>name = {item.title} </p>
-                        <img src={item.title + ".jpg"} width="200" length="200" />
-                        <p>showTime = {item.showTime} </p>
-                    </>)
-            })
-            : null}
-            </ul>
+            <div className="input-group">
+                <input type="text" value={val} onChange={(e) => setVal(e.target.value)} className="form-control" placeholder="Search for a movie, e.g. Black Panther"></input>
+                <button onClick={Search} className="btn btn-dark" type="button">Search</button>
             </div>
+
+            {filtered.length === 0 ?
+                <ul>
+                    {Array.isArray(movie)
+                        ? movie.map(item => {
+                            return (
+                                <><p>name = {item.title} </p>
+                                    <img src={item.title + ".jpg"} width="200" length="200" />
+                                    <p>showTime = {item.showTime} </p>
+                                </>)
+                        })
+                        : null}
+                </ul> :
+
+                <ul>
+                    {Array.isArray(filtered)
+                        ? filtered.map(item => {
+                            return (
+                                <><p>name = {item.title} </p>
+                                    <img src={item.title + ".jpg"} width="200" length="200" />
+                                    <p>showTime = {item.showTime} </p>
+                                </>)
+                        })
+                        : null}
+                </ul>
+
+            }
+
+
+
+
+        </div>
 
     );
 }
