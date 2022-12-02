50f0adf44a889af1e51cdddb659ba0fd1bed6c4d
Ryan_Ahlstrom <Ryan.Ahlstrom@gmail.com>
2022-12-02 13:53:39 -0700

    Fixed ContactUs Page

--- react-website-movies/src/components/pages/ContactUs.js
+++ react-website-movies/src/components/pages/ContactUs.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import emailjs from 'emailjs-com';
 
 export const ContactUs = () => {
   const form = useRef();
@@ -27,3 +27,4 @@ export const ContactUs = () => {
     </form>
   );
 };
+export default ContactUs;
