93f495e2efb774b0fd2c95e1fcd70d38bd5a9ab9
Dhruvidave1 <dhruvi@ualberta.ca>
2022-12-01 17:20:39 -0700

    added get request

--- react-website-movies/src/components/pages/AllMovies.js
+++ react-website-movies/src/components/pages/AllMovies.js
@@ -1,64 +1,86 @@
 import '../../App.css';
-import jsondata from './movie.json';
-import {Link, useNavigate} from 'react-router-dom';
-import React, { useState} from 'react';
-import { Navigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
 function AllMovies() {
     const [movie, setMovie] = useState([])
+    const [val, setVal] = useState('')
     let navigate = useNavigate();
-    
+
+    useEffect(() => {
+        const url = "http://localhost:8080/api/v1/movie";
+
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                const json = await response.json();
+                setMovie(json);
+                
+                            
+            } catch (error) {
+                console.log("error", error);
+            }
+        };
+        fetchData();
+        
+    }, []);
+
     const handleClick = (e) => {
         navigate('/login');
     }
 
+    const searchMovie = (e) => {
+        {
+            console.log(movie);  
+            movie.filter((val) => {
+                if (movie == "") {
+                    return val
+                } else if (val.title.toLowerCase().includes(movie.title.toLowerCase())) {
+                    return val
+                } return ""
+            }).map((val, key) => {
+                return <div>
+                    <button onClick={handleClick} type="button">{val.showTime}</button>
+                    <p></p>
+                    <img src={val.title + ".jpg"} width="200" length="200" />
+                </div>;
+            })
+        }
 
-    const searchMovie = async () => {
-        const url = '...' + movie;
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                Accept: 'application/json',
-            },
-        });
-        const result = await response.json();
-        setMovie(result);
-        setMovie('');        
+
     }
 
 
     return (
+
         <>
-        <div className='movie'>
-        
-            <div className="input-group">
-                <input type="text" value={movie} onChange={(e) => setMovie(e.target.value)} className="form-control" placeholder="Search for a movie, e.g. Black Panther"></input>                        
-                <button onClick={searchMovie} className="btn btn-dark" type="button">Search</button>                        
-            </div>
-            <p>Display the searched movie with showtimes below:</p>
-            <div>
-                {jsondata.filter((val)=>{
-                    if(movie == "") {
-                        return val
-                    } else if (val.movie_name.toLowerCase().includes(movie.toLowerCase())) {
-                        return val
-                    } return "" 
-                }).map((val, key)=>{
-                    return <div>
-                    <button onClick={handleClick} type="button">{val.movie_name}</button>
-                    {/* {val.movie_name}  */}
-                    <p></p>
-                    <img src={val.movie_name + ".jpg"} width="200" length ="200"/>
-                    </div>;
-                })}
-                
+            <div className='movie'>
+                <div className="input-group">
+                    <input type="text" value={val} onChange={(e) => setVal(e.target.value)} className="form-control" placeholder="Search for a movie, e.g. Black Panther"></input>
+                    <button className="btn btn-dark" type="button" onClick={searchMovie}>Search</button>
+                </div>
+
+                <ul>
+                    {Array.isArray(movie)
+                        ? movie.map(item => {
+                            return (
+                                <><p>name = {item.title} </p>
+                                    <img src={item.title + ".jpg"} width="200" length="200" />
+                                    <p>showTime = {item.showTime} </p>
+                                </>)
+                        })
+                        : null}
+                </ul>
+
+
+
+
             </div>
 
-        </div>
 
-        
 
         </>
+
     );
 }
 
