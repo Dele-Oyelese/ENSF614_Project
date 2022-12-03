062d4ddb80a173e6fae69dccafb2da7f1239d116
Ryan_Ahlstrom <Ryan.Ahlstrom@gmail.com>
2022-12-02 17:54:05 -0700

    Confirmation Email

--- react-website-movies/src/components/pages/Payment.js
+++ react-website-movies/src/components/pages/Payment.js
@@ -1,9 +1,11 @@
 import '../../App.css';
-import React from 'react';
+import React, { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from "react";
+import emailjs from 'emailjs-com';
 
 function Payment() {
+    const form = useRef();
 
     const [name, setname] = useState("");
     const [address, setaddress] = useState("");
@@ -21,7 +23,6 @@ function Payment() {
                 const json = await response.json();
                 setUser(json);
                 console.log(user);
-
             } catch (error) {
                 console.log("error", error);
             }
@@ -29,53 +30,54 @@ function Payment() {
         fetchData();
         setticketId(ticketId + 1);
         console.log(ticketId);
-
     }, []);
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('gmail', 'template_7woudlf', e.target, 'GE7vkWWcHkEMc_LhW')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset()
+      };
 
     const purchaseTicket = (e) => {
         e.preventDefault();
         const movieID = localStorage.getItem("id");
         const seatID = localStorage.getItem("seatid");
-
         if (localStorage.getItem("email") == null) {
-
             setFlag(0);
-
         }
-
         //t expects a 1 for registered User a 0 or anything else for a non registered user
-
         const url = "http://localhost:8080/api/v1/boxOffice/purchase/" + ticketId + "/movie/" + movieID + "/seat/" + seatID + "/ru/" + flag;
         console.log(url)
-
-
         fetch(url, {
             method: "PUT",
             headers: { "Content-Type": "application/json" },
         })
-
     }
 
     return (
         <>
             {/* @PutMapping("*/}
-
             <div className="movie">
-
                 <div>
-
-                    <p> Your selections are: </p>
-                    <p>Movie ID: {localStorage.getItem("id")}</p>
-                    <p>Movie Name: {localStorage.getItem("title")}</p>
-                    <p>Show Time: {localStorage.getItem("showTime")}</p>
-
-
+                <p> Your selections are: </p>
+                    <p name = "movie_id">Movie ID: {localStorage.getItem("id")}</p>
+                    <p name = "movie_name">Movie Name: {localStorage.getItem("title")}</p>
+                    <p name = "show_time">Show Time: {localStorage.getItem("showTime")}</p>
                 </div>
 
-                <form>
+                <form ref={form} onSubmit={sendEmail}>
+
+                    
+
                     <div className="form-group">
                         <label >Name</label>
                         <input
+                            name = "user_name"
                             defaultValue={user.name}
                             type="text"
                             onChange={(e) => setname(e.target.value)}
@@ -86,6 +88,7 @@ function Payment() {
                     <div className="form-group">
                         <label>Address</label>
                         <input
+                            name = "user_address"
                             type="text"
                             defaultValue={user.address}
                             onChange={(e) => setaddress(e.target.value)}
@@ -98,7 +101,8 @@ function Payment() {
                     <div className="form-group">
                         <label>Email</label>
                         <input
-                            type="text"
+                            name = "user_email"
+                            type="email"
                             defaultValue={user.email}
                             onChange={(e) => setemail(e.target.value)}
                             className="form-control"
@@ -116,16 +120,39 @@ function Payment() {
                             placeholder="Enter credit card number: e.g 1234 5678 9101 1213"
                         ></input>
                     </div>
+                    <input
+                        type="hidden"
+                        className="form-control"
+                        name="movie_id"
+                         defaultValue={localStorage.getItem("id")}
+                        ></input>
+                        <input
+                        type="hidden"
+                        className="form-control"
+                        name="movie_name"
+                         defaultValue={localStorage.getItem("title")}
+                         ></input>
+                        <input
+                        type="hidden"
+                        className="form-control"
+                        name="show_time"
+                         defaultValue={localStorage.getItem("showTime")}
+                         ></input>
+                         <input
+                        type="hidden"
+                        className="form-control"
+                        name="movie_id"
+                         defaultValue={localStorage.getItem("seatid")}
+                         ></input>
 
-                    <button
-                        type="submit"
-                        className="btn btn-primary"
-                        onClick={purchaseTicket}
-                    >
+                    <button type="submit" className="btn btn-primary" onClick={purchaseTicket}>
                         Purchase Ticket
                     </button>
+                    <p></p>
+                    <button type="submit" className="btn btn-primary" onClick={sendEmail}>
+                        Send Confirmation Email
+                    </button>
                 </form>
-
             </div>
         </>
     )
