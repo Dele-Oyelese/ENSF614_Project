b499e06385ab5a5b3d17280fd090ea541caaf2eb
Dhruvidave1 <dhruvi@ualberta.ca>
2022-12-04 16:49:03 -0700

    should work

--- react-website-movies/src/components/pages/CancelTicket.js
+++ react-website-movies/src/components/pages/CancelTicket.js
@@ -10,102 +10,93 @@ function CancelTicket() {
     const [buyerStatus, setStatus] = useState('');
     const [showTime, setshowtime] = useState('');
     const [email, setEmail] = useState('')
-    const now = new Date();    
-    const dayTime = 24 * 60 * 60 * 1000;   
+    const now = new Date();
+    const dayTime = 24 * 60 * 60 * 1000;
 
     const handleClick = (e) => {
         e.preventDefault();
 
         fetch("http://localhost:8080/api/v1/ticket/" + ticketId)
 
-        .then(res => res.json())
+            .then(res => res.json())
 
-        .then((result) => {
+            .then((result) => {
 
-            setticket(result);
+                setticket(result);
 
-            console.log(result);
-            console.log(result.boxOffices[0].movie.showTime)
-            console.log(ticket)
-            setStatus(
-                Array.isArray(result)
-                    ? result.map(item => {
-                        return (item.buyerStatus)
-                    })
-                    : null
-            )
+                console.log(result);
+                console.log(result.boxOffices[0].movie.showTime)
+                console.log(ticket)
+                setStatus(
+                    Array.isArray(ticket)
+                        ? ticket.map(item => {
+                            return (item.buyerStatus)
+                        })
+                        : null
+                )
 
-            setshowtime(
-                Array.isArray(result)
-                    ? result.map(item => {      
-                        console.log(item.boxOffices[0].movie.showTime)
-                        return (item.boxOffices[0].movie.showTime)
-                    })
-                    : null
-            )
+                setshowtime(
+                    Array.isArray(result)
+                        ? result.map(item => {
+                            console.log(item.boxOffices[0].movie.showTime)
+                            return (item.boxOffices[0].movie.showTime)
+                        })
+                        : null
+                )
 
-            if(result.boxOffices[0].movie.showTime != null){
-                const date = result.boxOffices[0].movie.showTime;
-                const stringDate =date.substring(0,10);
-                const hr = date.substring(12,13)
-                const min =date.substring(15,16)
-                const sec =date.substring(18,19)
-                console.log(stringDate)
-                console.log(hr)
+                if (result.boxOffices[0].movie.showTime != null) {
+                    const date = result.boxOffices[0].movie.showTime;
+                    const stringDate = date.substring(0, 10);
+                    const hr = date.substring(12, 13)
+                    const min = date.substring(15, 16)
+                    const sec = date.substring(18, 19)
+                    console.log(stringDate)
+                    console.log(hr)
 
-                const myArray = stringDate.split("-");
-                const payDate = new Date()
-                console.log(myArray)
-                payDate.setFullYear(myArray[0],myArray[1]-1,myArray[2])
-                payDate.setHours(hr,min,sec)
-                console.log(payDate)
-                const timeDiff = (payDate - now) / dayTime;
+                    const myArray = stringDate.split("-");
+                    const payDate = new Date()
+                    console.log(myArray)
+                    payDate.setFullYear(myArray[0], myArray[1] - 1, myArray[2])
+                    payDate.setHours(hr, min, sec)
+                    console.log(payDate)
+                    const timeDiff = (payDate - now) / dayTime;
+                    console.log(timeDiff);
 
-                // 
+                    if (timeDiff >= 3) {
+                        alert("Sorry, it has already been 72 hours from your purchased time. No refund can be processed");
+                    } else {
+                        //first deleting the ticket 
+                        const url = "http://localhost:8080/api/v1/boxOffice/cancel/" + ticketId;
+                        console.log(url)
+                        fetch(url, {
+                            method: "PUT",
+                            headers: { "Content-Type": "application/json" },
+                        })
+
+                        if (buyerStatus == true) {
+                            alert("Please type your email to recieve your coupon valued at $10");
+                        }
+
+                        if (buyerStatus == false) {
+                            alert("Please type your email to recieve your coupon valued at $8");
+                        }
+                    }
                 }
+            })
 
-
-
-        }
-        
-        )
-
-
-       
-
-        // if (timeDiff >= 3) {
-        //     alert("Sorry, it has already been 72 hours from your purchased time. No refund can be processed");
-        // } else {
-        //     //first deleting the ticket 
-        //     const url = "http://localhost:8080/api/v1/boxOffice/cancel/" + ticketId;
-        //     console.log(url)
-        //     fetch(url, {
-        //         method: "PUT",
-        //         headers: { "Content-Type": "application/json" },
-        //     })
-
-        //     if (buyerStatus == true) {
-        //         alert("A refund coupon of $9 has been sent to your email");
-        //     }
-
-        //     if (buyerStatus == false) {
-        //         alert("A refund coupon of $8 has been sent to your email");
-        //     }
-        // }
-
-    }    
+    }
 
     const form = useRef();
 
     const sendEmail = (e) => {
         e.preventDefault();
         emailjs.sendForm('gmail', 'template_7woudlf', form.current, 'GE7vkWWcHkEMc_LhW')
-          .then((result) => {
-              console.log(result.text);
-          }, (error) => {
-              console.log(error.text);
-          });
-      };
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
 
     return (
         <>
@@ -121,31 +112,34 @@ function CancelTicket() {
                             onChange={(e) => setticketid(e.target.value)}
                             placeholder="Enter your ticket ID number, e.g. 69907"></input>
                     </div>
+
                     <button
                         type="submit"
                         onClick={handleClick}
                         class="btn btn-primary" >Request Cancellation</button>
-                       <br></br>
-                       <input
-                            type="text"
-                            class="form-control"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            placeholder="Enter your email address, e.g. john.smith@gmail.com"></input>
-                       <button
+                    <br></br>
+                    <hr></hr>
+                    <input
+                        type="text"
+                        class="form-control"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Enter your email address, e.g. john.smith@gmail.com"></input>
+                    <br></br>
+                    <button
                         type="submit"
                         onClick={sendEmail}
-                        class="btn btn-primary" >Send Email Confirmation</button> 
+                        class="btn btn-primary" >Send Email Confirmation</button>
                 </form>
             </div>
             <form ref={form} onSubmit={sendEmail}>
-      <input type="hidden" name="user_name" defaultValue={"Greetings "}/>
-      <input type="hidden" name="user_email" defaultValue={email}/>
-      <input type="hidden" name="subject" defaultValue={"Movie Theater Cancellation Confirmation"}/>
-      <input type="hidden" name="message" defaultValue={"Confirming your cancelation for ticketID: " + ticketId + ", in the future please use the follwing Coupon Code for 15% off your next purchase: 15MOVIE15"} />
-      <input type="hidden" name="messageone" defaultValue={"We hope to see you at the movies"}/>
-    </form>
-           
+                <input type="hidden" name="user_name" defaultValue={"Greetings "} />
+                <input type="hidden" name="user_email" defaultValue={email} />
+                <input type="hidden" name="subject" defaultValue={"Movie Theater Cancellation Confirmation"} />
+                <input type="hidden" name="message" defaultValue={"Confirming your cancelation for ticketID: " + ticketId + ", in the future please use the follwing Coupon Code for 15% off your next purchase: 15MOVIE15"} />
+                <input type="hidden" name="messageone" defaultValue={"We hope to see you at the movies"} />
+            </form>
+
 
         </>
     );
