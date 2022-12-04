6140bd5fa4874d7171b3aa154987a40c51e99c6e
Dhruvidave1 <dhruvi@ualberta.ca>
2022-12-04 13:24:49 -0700

    payment

--- react-website-movies/src/components/pages/Payment.js
+++ react-website-movies/src/components/pages/Payment.js
@@ -50,12 +50,17 @@ function Payment(props) {
         console.log("TicketID is: " + props.ticketValue);
         console.log("Flag is" + flag);
         const url = "http://localhost:8080/api/v1/boxOffice/purchase/" + props.ticketValue + "/movie/" + movieID + "/seat/" + seatID + "/ru/" + flag;
-        props.updateTicket()     
-           
+        props.updateTicket()           
         fetch(url, {
             method: "PUT",
             headers: { "Content-Type": "application/json" },
-        })
+        }).then(() => {
+            alert("Ticket successfully added");            
+            setname("");
+            setaddress("");
+            setcreditcard("");
+            setemail("");
+          })
         
     }
 
