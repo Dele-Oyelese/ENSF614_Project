4e2fc7304bd54d4b44d8f7b9a5a1f455962be704
Dhruvidave1 <dhruvi@ualberta.ca>
2022-12-04 11:14:03 -0700

    app.js updated

--- react-website-movies/src/App.js
+++ react-website-movies/src/App.js
@@ -10,9 +10,15 @@ import Seating from './components/Seating';
 import Payment from './components/pages/Payment';
 import Admin from './components/pages/Admin';
 import FeeRenewal from './components/pages/FeeRenewal';
-
+import { useState, useEffect } from "react";
 
 function App() {
+  const [ticketId, setticketid] = useState(1);
+
+  const updateTicket = (e) => {
+    setticketid(ticketId+1)
+  }
+
   return (
     <>
       <Router>
@@ -26,6 +32,8 @@ function App() {
           <Route path='/seating' element={<Seating />} />
           <Route path='/payment' element={<Payment />} />
           <Route path='/admin' element={<Admin />} />
+          <Route path='/payment' element={<Payment updateTicket = {updateTicket} ticketValue = {ticketId}/>} />
+          <Route path='/contactus' element={<ContactUs />} />
           <Route path='/feeRenewal' element={<FeeRenewal />} />
           
         </Routes>
